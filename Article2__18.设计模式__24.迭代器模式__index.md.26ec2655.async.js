0d18b5e81d6c5f62f6e5abcbe9afa247bdf61085
eternallycyf <eternallycyf@users.noreply.github.com>
2022-09-23 06:36:34 +0000

    deploy: 284d7cb9a75d42254dba2a796b7b1b4fe682ab41

---
+++ Article2__18.设计模式__24.迭代器模式__index.md.26ec2655.async.js
@@ -0,0 +1,55 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{rbp7:function(l,o,e){"use strict";e.r(o);var i=e("cDcd"),t=e.n(i),n=e("dEAq"),d=e.n(n),s=e("6T1g");const E=t.a.memo(({demos:u})=>t.a.createElement(t.a.Fragment,null,t.a.createElement("div",{className:"markdown"},t.a.createElement("h2",{id:"\u8FED\u4EE3\u5668\u6A21\u5F0F"},t.a.createElement(n.AnchorLink,{to:"#\u8FED\u4EE3\u5668\u6A21\u5F0F","aria-hidden":"true",tabIndex:-1},t.a.createElement("span",{className:"icon icon-link"})),"\u8FED\u4EE3\u5668\u6A21\u5F0F"),t.a.createElement("ul",null,t.a.createElement("li",null,"\u8FED\u4EE3\u5668\u6A21\u5F0F",t.a.createElement("code",null,"Iterator Pattern"),"\u63D0\u4F9B\u4E86\u4E00\u79CD\u65B9\u6CD5\u987A\u5E8F\u8BBF\u95EE\u4E00\u4E2A\u805A\u5408\u5BF9\u8C61\u4E2D\u7684\u5404\u4E2A\u5143\u7D20\uFF0C\u800C\u53C8\u65E0\u9700\u66B4\u9732\u8BE5\u5BF9\u8C61\u7684\u5185\u90E8\u5B9E\u73B0"),t.a.createElement("li",null,"\u8FD9\u6837\u65E2\u53EF\u4EE5\u505A\u5230\u4E0D\u66B4\u9732\u96C6\u5408\u7684\u5185\u90E8\u7ED3\u6784\uFF0C\u53C8\u53EF\u8BA9\u5916\u90E8\u4EE3\u7801\u900F\u660E\u5730\u8BBF\u95EE\u96C6\u5408\u5185\u90E8\u7684\u6570\u636E\uFF0C\u8FED\u4EE3\u5668\u6A21\u5F0F\u5C5E\u4E8E\u884C\u4E3A\u578B\u6A21\u5F0F\u3002")),t.a.createElement("h2",{id:"\u4EE3\u7801\u5B9E\u73B0"},t.a.createElement(n.AnchorLink,{to:"#\u4EE3\u7801\u5B9E\u73B0","aria-hidden":"true",tabIndex:-1},t.a.createElement("span",{className:"icon icon-link"})),"\u4EE3\u7801\u5B9E\u73B0"),t.a.createElement(s.a,{code:`// \u5E7F\u64AD\u7535\u53F0\u793A\u4F8B
+
+class RadioStation {
+  // \u7535\u53F0
+  constructor(frequency) {
+    this.frequency = frequency;
+  }
+
+  getFrequency() {
+    return this.frequency;
+  }
+}
+
+class StationList {
+  // \u8FED\u4EE3\u5668
+  constructor() {
+    this.index = -1;
+    this.stations = [];
+  }
+
+  get(i) {
+    return this.stations[this.index];
+  }
+
+  hasNext() {
+    let index = this.index + 1;
+    return this.stations[index] !== void 0;
+  }
+
+  next() {
+    return this.stations[++this.index];
+  }
+
+  addStation(station) {
+    this.stations.push(station);
+  }
+
+  removeStation(toRemove) {
+    const toRemoveFrequency = toRemove.getFrequency();
+    this.stations = this.stations.filter(
+      station => station.getFrequency() !== toRemoveFrequency,
+    );
+  }
+}
+
+(function() {
+  const stationList = new StationList();
+  stationList.addStation(new RadioStation(89));
+  stationList.addStation(new RadioStation(101));
+  stationList.addStation(new RadioStation(102));
+  stationList.addStation(new RadioStation(103.2));
+  stationList.stations.forEach(station => console.log(station.getFrequency())); // 89 101 102 103.2
+  stationList.removeStation(new RadioStation(89));
+  while (stationList.hasNext()) console.log(stationList.next().getFrequency()); // 101 102 103.2
+})();`,lang:"js"}),t.a.createElement("h2",{id:"\u53C2\u8003\u94FE\u63A5"},t.a.createElement(n.AnchorLink,{to:"#\u53C2\u8003\u94FE\u63A5","aria-hidden":"true",tabIndex:-1},t.a.createElement("span",{className:"icon icon-link"})),"\u53C2\u8003\u94FE\u63A5"),t.a.createElement("ul",null,t.a.createElement("li",null,t.a.createElement(n.Link,{to:"https://www.cnblogs.com/xuwendong/p/9898030.html"},"https://www.cnblogs.com/xuwendong/p/9898030.html")),t.a.createElement("li",null,t.a.createElement(n.Link,{to:"https://www.runoob.com/design-pattern/iterator-pattern.html"},"https://www.runoob.com/design-pattern/iterator-pattern.html")),t.a.createElement("li",null,t.a.createElement(n.Link,{to:"https://github.com/sohamkamani/javascript-design-patterns-for-humans#-iterator"},"https://github.com/sohamkamani/javascript-design-patterns-for-humans#-iterator"))))));o.default=u=>{const r=t.a.useContext(n.context),c=r.demos;return t.a.useEffect(()=>{var a;u!=null&&(a=u.location)!==null&&a!==void 0&&a.hash&&n.AnchorLink.scrollToAnchor(decodeURIComponent(u.location.hash.slice(1)))},[]),t.a.createElement(E,{demos:c})}}}]);
