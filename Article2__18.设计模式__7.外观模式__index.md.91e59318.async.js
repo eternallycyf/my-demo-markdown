450aa54031578fc1bc3b6e5f68f69f8d92f7a592
eternallycyf <a969475322@gmail.com>
2022-09-27 10:34:49 +0800

    Updates

---
+++ Article2__18.设计模式__7.外观模式__index.md.91e59318.async.js
@@ -0,0 +1,14 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{wmDp:function(i,l,u){"use strict";u.r(l);var c=u("cDcd"),e=u.n(c),t=u("dEAq"),m=u.n(t),E=u("H1Ra");const o=e.a.memo(({demos:a})=>e.a.createElement(e.a.Fragment,null,e.a.createElement("div",{className:"markdown"},e.a.createElement("h2",{id:"\u5916\u89C2\u6A21\u5F0F"},e.a.createElement(t.AnchorLink,{to:"#\u5916\u89C2\u6A21\u5F0F","aria-hidden":"true",tabIndex:-1},e.a.createElement("span",{className:"icon icon-link"})),"\u5916\u89C2\u6A21\u5F0F"),e.a.createElement("ul",null,e.a.createElement("li",null,"\u5916\u89C2\u6A21\u5F0F\u901A\u8FC7\u5B9A\u4E49\u4E00\u4E2A\u4E00\u81F4\u7684\u63A5\u53E3\uFF0C\u7528\u4EE5\u5C4F\u853D\u5185\u90E8\u5B50\u7CFB\u7EDF\u7684\u7EC6\u8282"),e.a.createElement("li",null,"\u4F7F\u5F97\u8C03\u7528\u7AEF\u53EA\u9700\u8DDF\u8FD9\u4E2A\u63A5\u53E3\u53D1\u751F\u8C03\u7528\uFF0C\u800C\u65E0\u9700\u5173\u5FC3\u8FD9\u4E2A\u5B50\u7CFB\u7EDF\u7684\u5185\u90E8\u7EC6\u8282")),e.a.createElement("h2",{id:"\u4EE3\u7801\u5B9E\u73B0"},e.a.createElement(t.AnchorLink,{to:"#\u4EE3\u7801\u5B9E\u73B0","aria-hidden":"true",tabIndex:-1},e.a.createElement("span",{className:"icon icon-link"})),"\u4EE3\u7801\u5B9E\u73B0"),e.a.createElement(E.a,{code:`class Family {
+  static main(): void {
+    const father = new Father();
+    const child = new Father();
+    father.work();
+    child.study();
+  }
+}
+class Father {
+  work() {}
+}
+class Child {
+  study() {}
+}`,lang:"typescript"}),e.a.createElement("h2",{id:"\u53C2\u8003\u94FE\u63A5"},e.a.createElement(t.AnchorLink,{to:"#\u53C2\u8003\u94FE\u63A5","aria-hidden":"true",tabIndex:-1},e.a.createElement("span",{className:"icon icon-link"})),"\u53C2\u8003\u94FE\u63A5"),e.a.createElement("ul",null,e.a.createElement("li",null,e.a.createElement(t.Link,{to:"https://segmentfault.com/a/1190000014132789"},"https://segmentfault.com/a/1190000014132789")),e.a.createElement("li",null,e.a.createElement(t.Link,{to:"https://www.runoob.com/design-pattern/facade-pattern.html"},"https://www.runoob.com/design-pattern/facade-pattern.html")),e.a.createElement("li",null,e.a.createElement(t.Link,{to:"https://design-patterns.readthedocs.io/zh_CN/latest/structural_patterns/facade.html"},"https://design-patterns.readthedocs.io/zh_CN/latest/structural_patterns/facade.html"))))));l.default=a=>{const r=e.a.useContext(t.context),s=r.demos;return e.a.useEffect(()=>{var n;a!=null&&(n=a.location)!==null&&n!==void 0&&n.hash&&t.AnchorLink.scrollToAnchor(decodeURIComponent(a.location.hash.slice(1)))},[]),e.a.createElement(o,{demos:s})}}}]);
