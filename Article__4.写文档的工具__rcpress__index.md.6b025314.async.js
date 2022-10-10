66cde8f0f254c5eda4e7eb527336d7e3e93de63b
eternallycyf <a969475322@gmail.com>
2022-10-10 15:54:00 +0800

    Updates

---
+++ Article__4.写文档的工具__rcpress__index.md.6b025314.async.js
@@ -0,0 +1,32 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{KbXv:function(m,s,a){"use strict";a.r(s);var r=a("cDcd"),e=a.n(r),n=a("dEAq"),u=a.n(n),c=a("H1Ra");const l=e.a.memo(({demos:t})=>e.a.createElement(e.a.Fragment,null,e.a.createElement("div",{className:"markdown"},e.a.createElement("h1",{id:"reactpress"},e.a.createElement(n.AnchorLink,{to:"#reactpress","aria-hidden":"true",tabIndex:-1},e.a.createElement("span",{className:"icon icon-link"})),"reactPress"),e.a.createElement("h2",{id:"\u51C6\u5907\u9636\u6BB5"},e.a.createElement(n.AnchorLink,{to:"#\u51C6\u5907\u9636\u6BB5","aria-hidden":"true",tabIndex:-1},e.a.createElement("span",{className:"icon icon-link"})),"\u51C6\u5907\u9636\u6BB5"),e.a.createElement(c.a,{code:`npm install @rcpress/cli
+"start": "rcpress dev docs",
+"build": "rcpress build docs"
+"generate": "rcpress generate docs"
+# \u76EE\u5F55\u7ED3\u6784
+\u251C\u2500\u2500 docs # \u5B58\u653E\u6587\u6863\u76EE\u5F55
+    \u251C\u2500\u2500 .rcpress
+        \u251C\u2500\u2500 globalComponent.js # \u5168\u5C40\u7EC4\u4EF6
+        \u251C\u2500\u2500 config.js
+        \u251C\u2500\u2500 style.less # \u5168\u5C40\u6837\u5F0F
+        \u251C\u2500\u2500 public # \u9759\u6001\u6587\u4EF6\uFF0C\u6587\u4EF6\u53EF\u4EE5\u76F4\u63A5\u8BBF\u95EE
+        \u251C\u2500\u2500 theme
+    \u251C\u2500\u2500 guide
+        \u251C\u2500\u2500 README.MD
+    \u251C\u2500\u2500 README.MD
+\u251C\u2500\u2500 package.json
+#
+build\u7684\u751F\u4EA7\u6A21\u5F0F\u8DEF\u5F84\u6709\u95EE\u9898
+\u4F7F\u7528 "rcpress generate docs" \u7684\u5F62\u5F0F`,lang:"js"}),e.a.createElement("h2",{id:"configjs"},e.a.createElement(n.AnchorLink,{to:"#configjs","aria-hidden":"true",tabIndex:-1},e.a.createElement("span",{className:"icon icon-link"})),"config.js"),e.a.createElement(c.a,{code:`#
+module.exports = {
+  logo: '/favicon.png',
+  themeConfig:{
+    nav:[
+      {
+        text:"guide",
+        link:'/xxx',
+        important:true // \u6709\u7EA2\u8272\u6807\u8BB0\u663E\u793A
+      }
+    ]
+  }
+}`,lang:"js"}),e.a.createElement("h3",{id:"markdown"},e.a.createElement(n.AnchorLink,{to:"#markdown","aria-hidden":"true",tabIndex:-1},e.a.createElement("span",{className:"icon icon-link"})),"markdown"),e.a.createElement(c.a,{code:`::: tip |
+:::`,lang:"unknown"}))));s.default=t=>{const i=e.a.useContext(n.context),d=i.demos;return e.a.useEffect(()=>{var o;t!=null&&(o=t.location)!==null&&o!==void 0&&o.hash&&n.AnchorLink.scrollToAnchor(decodeURIComponent(t.location.hash.slice(1)))},[]),e.a.createElement(l,{demos:d})}}}]);
---
+++ Project__VueHOC__index.md.37b5a531.async.js
@@ -0,0 +1,283 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[114],{ZQst:function(u,o,e){"use strict";e.r(o);var l=e("cDcd"),n=e.n(l),t=e("dEAq"),p=e.n(t),a=e("H1Ra");const i=n.a.memo(({demos:s})=>n.a.createElement(n.a.Fragment,null,n.a.createElement("div",{className:"markdown"},n.a.createElement("h1",{id:"vuehoc"},n.a.createElement(t.AnchorLink,{to:"#vuehoc","aria-hidden":"true",tabIndex:-1},n.a.createElement("span",{className:"icon icon-link"})),"VueHOC"),n.a.createElement("h2",{id:"vue3"},n.a.createElement(t.AnchorLink,{to:"#vue3","aria-hidden":"true",tabIndex:-1},n.a.createElement("span",{className:"icon icon-link"})),"Vue3"),n.a.createElement("ul",null,n.a.createElement("li",null,"utils.tsx")),n.a.createElement(a.a,{code:`import { reactive, onMounted, unref, defineComponent, watch } from 'vue';
+/**
+ * HOC Component transform function
+ * @param {(params: any) => Promise<any>} promiseFn
+ * @param {Object} params
+ */
+const usePromiseFetch = (
+  promiseFn: (params: any) => Promise<any>,
+  params = {},
+) => {
+  const obj = reactive<{ result: null | {}; error: boolean; loading: boolean }>(
+    {
+      result: null,
+      error: false,
+      loading: false,
+    },
+  );
+
+  const fetchRequest = async params => {
+    obj.loading = true;
+    const result = await promiseFn(params).finally(() => {
+      obj.loading = false;
+    });
+    obj.result = result;
+  };
+
+  /**
+   * @param {JSX.IntrinsicElements | any} Wrapped
+   */
+  return function wrap(Wrapped: JSX.IntrinsicElements | any) {
+    onMounted(() => fetchRequest(params));
+    watch(params, newVal => fetchRequest(newVal), { immediate: true });
+
+    const NewWrapped = defineComponent<InstanceType<typeof Wrapped>>({
+      setup(props, ctx) {
+        return {
+          slots: ctx.slots,
+          attrs: ctx.attrs,
+          props,
+          this: this,
+        };
+      },
+      render() {
+        if (obj.loading) return <span>loading...</span>;
+        if (obj.error) return <span>error...</span>;
+        return (
+          <Wrapped
+            slots={this.slots}
+            attrs={this.attrs}
+            props={this.props}
+            loading={unref(obj.loading)}
+            result={unref(obj.result)}
+          />
+        );
+      },
+    });
+    return NewWrapped;
+  };
+};
+
+function compose(...funcs: Function[]) {
+  if (funcs.length === 0) {
+    return <T extends any>(arg: T) => arg;
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0];
+  }
+
+  return funcs.reduce((a, b) => (...args: any) => a(b(...args)));
+}
+
+function normalizeProps(vm) {
+  return {
+    on: vm.$listeners,
+    attr: vm.$attrs,
+    scopedSlots: vm.$scopedSlots,
+  };
+}
+
+const WithLog = (Wrapped: JSX.IntrinsicElements | any) => {
+  return defineComponent({
+    mounted() {
+      console.log('log');
+    },
+    render() {
+      const props = normalizeProps(this);
+      return <Wrapped {...props} />;
+    },
+  });
+};
+
+export { usePromiseFetch, compose, WithLog };`,lang:"tsx"}),n.a.createElement("ul",null,n.a.createElement("li",null,"View.vue")),n.a.createElement(a.a,{code:`<template>
+  <div>
+    <span>{{ result?.name }}</span>
+  </div>
+</template>
+<script setup lang="tsx">
+  import { useSlots, useAttrs } from 'vue';
+  const { result, loading } = defineProps(['result', 'loading']);
+
+  const slots = useSlots();
+  const attrs = useAttrs();
+  defineExpose({
+    slots,
+    attrs,
+  });
+</script>`,lang:"tsx"}),n.a.createElement("ul",null,n.a.createElement("li",null,"index.vue")),n.a.createElement(a.a,{code:`<template> <HOC /></template>
+<script setup lang="tsx">
+  import { compose, usePromiseFetch, WithLog } from './utils';
+  import View from './View.vue';
+
+  const params = { id: 10 };
+
+  const request = (params = {}) => {
+    // mock the fetch
+    // parmas is the params of the request
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve({ name: 'ssh' });
+      }, 1000);
+    });
+  };
+
+  // const HOC = usePromiseFetch(View, request, params);
+  // const HOC = WithLog(usePromiseFetch(View, request, params));
+  const composed = compose(usePromiseFetch(request), WithLog);
+  const HOC = composed(View);
+</script>`,lang:"html"}),n.a.createElement("h2",{id:"vue2"},n.a.createElement(t.AnchorLink,{to:"#vue2","aria-hidden":"true",tabIndex:-1},n.a.createElement("span",{className:"icon icon-link"})),"vue2"),n.a.createElement(a.a,{code:`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>hoc-promise</title>
+  </head>
+  <body>
+    <div id="app">
+      <hoc msg="msg" @change="onChange">
+        <template>
+          <div>I am slot</div>
+        </template>
+        <template v-slot:named>
+          <div>I am named slot</div>
+        </template>
+      </hoc>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.7.10"></script>
+    <script>
+      var view = {
+        props: ['result'],
+        data() {
+          return {
+            requestParams: {
+              name: 'ssh',
+            },
+          };
+        },
+        methods: {
+          reload() {
+            this.requestParams = {
+              name: 'changed!!',
+            };
+          },
+        },
+        template: \`
+          <span>
+            <span>{{result?.name}}</span>
+            <slot></slot>
+            <slot name="named"></slot>
+            <button @click="reload">\u91CD\u65B0\u52A0\u8F7D\u6570\u636E</button>
+          </span>
+        \`,
+      };
+
+      const withPromise = promiseFn => {
+        return function(wrapped) {
+          return {
+            data() {
+              return {
+                loading: false,
+                error: false,
+                result: null,
+              };
+            },
+            methods: {
+              async request() {
+                this.loading = true;
+                // \u4ECE\u5B50\u7EC4\u4EF6\u5B9E\u4F8B\u91CC\u62FF\u5230\u6570\u636E
+                const { requestParams } = this.$refs.wrapped;
+                // \u4F20\u9012\u7ED9\u8BF7\u6C42\u51FD\u6570
+                const result = await promiseFn(requestParams).finally(() => {
+                  this.loading = false;
+                });
+                this.result = result;
+              },
+            },
+            async mounted() {
+              // \u7ACB\u523B\u53D1\u9001\u8BF7\u6C42\uFF0C\u5E76\u4E14\u76D1\u542C\u53C2\u6570\u53D8\u5316\u91CD\u65B0\u8BF7\u6C42
+              this.$refs.wrapped.$watch(
+                'requestParams',
+                this.request.bind(this),
+                {
+                  immediate: true,
+                },
+              );
+            },
+            render(h) {
+              const args = {
+                props: {
+                  // \u6DF7\u5165 $attrs
+                  ...this.$attrs,
+                  result: this.result,
+                  loading: this.loading,
+                },
+
+                // \u4F20\u9012\u4E8B\u4EF6
+                on: this.$listeners,
+
+                // \u4F20\u9012 $scopedSlots
+                scopedSlots: this.$scopedSlots,
+                ref: 'wrapped',
+              };
+
+              const wrapper = h('div', [
+                this.loading ? h('span', ['\u52A0\u8F7D\u4E2D\u2026\u2026']) : null,
+                this.error ? h('span', ['\u52A0\u8F7D\u9519\u8BEF']) : null,
+                h(wrapped, args),
+              ]);
+
+              return wrapper;
+            },
+          };
+        };
+      };
+
+      const withLog = wrapped => {
+        return {
+          mounted() {
+            console.log('I am mounted!');
+          },
+          render(h) {
+            return h(wrapped, normalizeProps(this));
+          },
+        };
+      };
+
+      const request = data => {
+        return new Promise(r => {
+          setTimeout(() => {
+            r(data);
+          }, 1000);
+        });
+      };
+
+      const composed = compose(withLog, withPromise(request));
+
+      var hoc = composed(view);
+
+      new Vue({
+        el: '#app',
+        components: {
+          hoc,
+        },
+        methods: {
+          onChange() {},
+        },
+      });
+
+      function compose(...funcs) {
+        return funcs.reduce((a, b) => (...args) => a(b(...args)));
+      }
+
+      function normalizeProps(vm) {
+        return {
+          on: vm.$listeners,
+          attr: vm.$attrs,
+          // \u4F20\u9012 $scopedSlots
+          scopedSlots: vm.$scopedSlots,
+        };
+      }
+    </script>
+  </body>
+</html>`,lang:"html"}))));o.default=s=>{const c=n.a.useContext(t.context),m=c.demos;return n.a.useEffect(()=>{var r;s!=null&&(r=s.location)!==null&&r!==void 0&&r.hash&&t.AnchorLink.scrollToAnchor(decodeURIComponent(s.location.hash.slice(1)))},[]),n.a.createElement(i,{demos:m})}}}]);
