25c0db786b267945fe62aaffb1fa18d55395b2e7
eternallycyf <a969475322@gmail.com>
2022-10-30 21:35:33 +0800

    feat:CustTooltip  add multiple row type

--- src/Components/BusinessComponent/CustTooltip/CustTooltip.tsx
+++ src/Components/BusinessComponent/CustTooltip/CustTooltip.tsx
@@ -1,40 +1,61 @@
 import React from 'react';
-import { Tooltip } from 'antd';
+import { Tooltip, Input, Col, Row } from 'antd';
+const { TextArea } = Input;
 
 interface Iprops {
   text: string;
   /**最大长度  */
   maxLength?: number;
-  style?: React.CSSProperties;
+  style?: React.CSSProperties | any;
   /**是否自动换行  */
-  isARow?: boolean;
+  autoSize?: boolean;
+  /**自动换行栅格 */
+  col?: number | string;
 }
 
 const CustTooltip: React.FC<Iprops> = props => {
-  const { text = '', maxLength = 35, style, isARow = false } = props;
-  if (isARow)
+  const {
+    text = '',
+    maxLength = 35,
+    style = {},
+    autoSize = false,
+    col = 8,
+  } = props;
+
+  const styles = {
+    maxWidth: 370,
+    wordWrap: 'break-word',
+    wordBreak: 'break-all',
+    color: 'rgba(0,0,0,0.45)',
+    fontSize: 14,
+    ...style,
+  };
+
+  if (autoSize) {
     return (
-      <span
-        style={{
-          maxWidth: 370,
-          wordWrap: 'break-word',
-          wordBreak: 'break-all',
-          color: 'rgba(0,0,0,0.45)',
-          ...style,
-        }}
-      >
-        {text ?? '--'}
-      </span>
+      <Col span={col}>
+        <TextArea
+          style={{ resize: 'none', ...style }}
+          autoSize
+          bordered={false}
+          readOnly
+          value={text ?? '--'}
+        />
+      </Col>
     );
+  }
+
+  const MAXCustTooltip = (
+    <Tooltip title={text} style={styles}>
+      <span style={styles}>{text.slice(0, maxLength) + '...' ?? '--'}</span>
+    </Tooltip>
+  );
+
+  const OriginText = <span style={styles}>{text ?? '--'}</span>;
+
   return (
-    <span style={style}>
-      {text && text.length > maxLength ? (
-        <Tooltip title={text} style={style}>
-          <span>{text.slice(0, maxLength) + '...' ?? '--'}</span>
-        </Tooltip>
-      ) : (
-        text ?? '--'
-      )}
+    <span style={styles}>
+      {text && text.length > maxLength ? MAXCustTooltip : OriginText}
     </span>
   );
 };
--- src/Components/BusinessComponent/CustTooltip/index.tsx
+++ src/Components/BusinessComponent/CustTooltip/index.tsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react';
 import CustTooltip from './CustTooltip';
+import { Tooltip, Input, Col, Row } from 'antd';
+
+const TEXT = 'sadasdasdasdasdsdsdsadasdasdasdasdsdsdsadasdasdasdasdsdsd';
 
 const IndexPage = () => {
   return (
     <>
-      <CustTooltip text="1213124124" maxLength={5} />
+      <Row gutter={10}>
+        <CustTooltip text={TEXT} autoSize={true} col={3} />
+        <CustTooltip text={TEXT} col={8} />
+        <CustTooltip text={TEXT} maxLength={10} />
+      </Row>
     </>
   );
 };
