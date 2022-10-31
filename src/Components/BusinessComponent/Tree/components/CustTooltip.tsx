7e617547aca653900a480d49b4996f1389d485b2
eternallycyf <a969475322@gmail.com>
2022-10-31 18:45:37 +0800

    test: test

--- src/Components/BusinessComponent/Tree/components/CustTooltip.tsx
+++ src/Components/BusinessComponent/Tree/components/CustTooltip.tsx
@@ -14,7 +14,6 @@ const CustTooltip: React.FC<Iprops> = props => {
   const { text = '', maxLength = 35, style, isRow = false } = props;
   if (isRow)
     return (
-
       <span
         style={{
           maxWidth: 370,
