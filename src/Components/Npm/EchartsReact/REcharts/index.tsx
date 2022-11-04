6907099549916aa15b0f4ba208263fc17e14b1b5
eternallycyf <a969475322@gmail.com>
2022-11-04 10:36:14 +0800

    feat: add echarts hover demo

--- src/Components/Npm/EchartsReact/REcharts/index.tsx
+++ src/Components/Npm/EchartsReact/REcharts/index.tsx
@@ -2,15 +2,19 @@ import { useRef, useImperativeHandle, forwardRef } from "react";
 import type { EChartsReactProps } from "echarts-for-react/lib/types";
 import ReactECharts from "echarts-for-react";
 
-const ReactEChart = (props: EChartsReactProps, echartsRef: any) => {
+interface IHandle {
+  ref: echarts.ECharts
+}
+
+const ReactEChart: React.ForwardRefRenderFunction<IHandle, EChartsReactProps> = (props, echartsRef: any) => {
+  const { option, ...restProps } = props;
   const ref = useRef<any>(null!);
   useImperativeHandle(echartsRef, () => ({
     ref: ref.current.getEchartsInstance(),
   }));
-
   return (
     <div ref={echartsRef}>
-      <ReactECharts ref={ref} {...props} />
+      <ReactECharts ref={ref} option={option} {...restProps} />
     </div>
   );
 };
--- src/Components/Npm/EchartsReact/demo.tsx
+++ src/Components/Npm/EchartsReact/demo.tsx
@@ -1,22 +1,3 @@
----
-order: 1
-group:
-  path: /npm
-  title: npm package
-  order: 1
----
-
-## echarts-for-react
-
-<code src="./index.tsx" transform={true} iframe={true}></code>
-
-<API src="./REcharts/index.tsx" hideTitle></API>
-
-## 手动触发 hover
-
-<code src="./demo.tsx" inline={true} transform={true} iframe={true}></code>
-
-```tsx | pure
 // echarts引入
 import * as echarts from 'echarts';
 import { useEffect, useRef, useState } from 'react';
@@ -27,11 +8,11 @@ const Echarts = () => {
   // echarts 配置项
   let option = {
     tooltip: {
-      trigger: 'item',
+      trigger: 'item'
     },
     legend: {
       top: '5%',
-      left: 'center',
+      left: 'center'
     },
     series: [
       {
@@ -42,76 +23,62 @@ const Echarts = () => {
         itemStyle: {
           borderRadius: 10,
           borderColor: '#fff',
-          borderWidth: 2,
+          borderWidth: 2
         },
         label: {
           show: false,
-          position: 'center',
+          position: 'center'
         },
         emphasis: {
           label: {
             show: true,
             fontSize: '40',
-            fontWeight: 'bold',
-          },
+            fontWeight: 'bold'
+          }
         },
         labelLine: {
-          show: false,
+          show: false
         },
         data: [
           { value: 1048, name: 'Search Engine' },
           { value: 735, name: 'Direct' },
           { value: 580, name: 'Email' },
           { value: 484, name: 'Union Ads' },
-          { value: 300, name: 'Video Ads' },
-        ],
-      },
-    ],
+          { value: 300, name: 'Video Ads' }
+        ]
+      }
+    ]
   };
   useEffect(() => {
     setTimeout(() => {
       // echarts 初始化
       chartDom?.current && setMyChart(echarts.init(chartDom?.current));
       myChart && option && myChart.setOption(option);
-    });
-  });
+    })
+  })
 
   return (
     <>
-      <div
-        ref={chartDom}
-        style={{
-          width: 800,
-          height: 800,
-          display: 'inline-block',
-          verticalAlign: 'top',
-        }}
-      ></div>
-      <div
-        style={{ display: 'inline-block', fontSize: '24px' }}
+      <div ref={chartDom} style={{ width: 800, height: 800, display: 'inline-block', verticalAlign: 'top' }}></div>
+      <div style={{ display: 'inline-block', fontSize: '24px' }}
         onMouseOver={() => {
           // 显示tooltip
-          myChart &&
-            myChart.dispatchAction({
-              type: 'showTip',
-              seriesIndex: 0,
-              dataIndex: 1,
-              position: 50,
-            });
+          myChart && myChart.dispatchAction({
+            type: 'showTip',
+            seriesIndex: 0,
+            dataIndex: 1,
+            position: 50
+          });
         }}
         onMouseLeave={() => {
           // 隐藏tooltip
-          myChart &&
-            myChart.dispatchAction({
-              type: 'hideTip',
-            });
+          myChart && myChart.dispatchAction({
+            type: 'hideTip',
+          });
         }}
-      >
-        1111
-      </div>
+      >1111</div>
     </>
   );
 };
 
 export default Echarts;
-```
