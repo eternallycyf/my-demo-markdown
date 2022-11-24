ba6087b650fd03a83569e906efaff8c5b64a100a
eternallycyf <a969475322@gmail.com>
2022-11-24 18:11:39 +0800

    feat: add waterMark component

---
+++ src/Components/BusinessComponent/WaterMark/index.md
@@ -0,0 +1,12 @@
+---
+order: 2
+group:
+  path: /my-components
+  title: 封装的业务组件
+  order: 2
+---
+
+## WaterMark 水印
+
+<code src="./index.tsx"  title='水印' desc=''></code>
+<API src="./WaterMark.tsx" ></API>
---
+++ src/Components/BusinessComponent/WaterMark/index.tsx
@@ -0,0 +1,10 @@
+import WaterMark from './WaterMark';
+const IndexPage = () => {
+  return (
+    <div style={{ height: 500 }}>
+      <WaterMark></WaterMark>
+    </div>
+  );
+};
+
+export default IndexPage;
