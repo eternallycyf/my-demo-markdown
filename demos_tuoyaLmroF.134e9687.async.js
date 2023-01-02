4a821f4ea173a5579df728e1e33e4296c9ca172f
eternallycyf <eternallycyf@users.noreply.github.com>
2023-01-02 06:08:13 +0000

    deploy: e95f3ba99942f10f46397e7d0a6e0f2c7f2d0d58

---
+++ demos_tuoyaLmroF.134e9687.async.js
@@ -0,0 +1,98 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[161],{KuHX:function(module,__webpack_exports__,__webpack_require__){"use strict";eval(`__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var antd_es_card_style__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("IzEo");
+/* harmony import */ var antd_es_card__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("bx4M");
+/* harmony import */ var antd_es_row_style__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__("14J3");
+/* harmony import */ var antd_es_row__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__("BMrR");
+/* harmony import */ var antd_es_col_style__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__("jCWc");
+/* harmony import */ var antd_es_col__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__("kPKH");
+/* harmony import */ var antd_es_input_style__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__("5NDa");
+/* harmony import */ var antd_es_input__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__("5rEg");
+/* harmony import */ var antd_es_space_style__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__("DYRE");
+/* harmony import */ var antd_es_space__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__("zeV3");
+/* harmony import */ var antd_es_form_style__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__("y8nQ");
+/* harmony import */ var antd_es_form__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__("Vl3Y");
+/* harmony import */ var antd_es_input_number_style__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__("giR+");
+/* harmony import */ var antd_es_input_number__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__("fyUT");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__("cDcd");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_14___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_14__);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const FormLayout = () => {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_14___default.a.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_card__WEBPACK_IMPORTED_MODULE_1__[/* default */ "a"], {
+    style: {
+      margin: 24
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement("h1", null, "1. ", "<space />", " "), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_form__WEBPACK_IMPORTED_MODULE_11__[/* default */ "a"].Item, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_space__WEBPACK_IMPORTED_MODULE_9__[/* default */ "b"], {
+    align: "baseline",
+    size: "large"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_space__WEBPACK_IMPORTED_MODULE_9__[/* default */ "b"], {
+    align: "baseline"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_form__WEBPACK_IMPORTED_MODULE_11__[/* default */ "a"].Item, {
+    name: "1"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_input_number__WEBPACK_IMPORTED_MODULE_13__[/* default */ "a"], {
+    min: 0,
+    max: 100
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement("span", null, "%")))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement("h1", null, "2. inline-flex"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_form__WEBPACK_IMPORTED_MODULE_11__[/* default */ "a"].Item, {
+    label: "\\u540D\\u79F0\\u4E00",
+    style: {
+      marginBottom: 0
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_form__WEBPACK_IMPORTED_MODULE_11__[/* default */ "a"].Item, {
+    style: {
+      display: "inline-flex",
+      width: "calc(45% - 4px)"
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_input__WEBPACK_IMPORTED_MODULE_7__[/* default */ "a"], null)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_form__WEBPACK_IMPORTED_MODULE_11__[/* default */ "a"].Item, {
+    style: {
+      display: "inline-flex",
+      width: "calc(55% - 4px)",
+      marginLeft: "8px"
+    },
+    name: "name2"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_input__WEBPACK_IMPORTED_MODULE_7__[/* default */ "a"], null)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_form__WEBPACK_IMPORTED_MODULE_11__[/* default */ "a"].Item, {
+    label: "InputNumber\\u8868\\u5355",
+    style: {
+      height: 32
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_space__WEBPACK_IMPORTED_MODULE_9__[/* default */ "b"], {
+    align: "baseline",
+    size: "large"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_space__WEBPACK_IMPORTED_MODULE_9__[/* default */ "b"], {
+    align: "baseline"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_form__WEBPACK_IMPORTED_MODULE_11__[/* default */ "a"].Item, {
+    name: "inputNumber1"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_input_number__WEBPACK_IMPORTED_MODULE_13__[/* default */ "a"], {
+    min: 0,
+    max: 100
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement("span", null, "%"))))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement("h1", null, "3. row and col"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_row__WEBPACK_IMPORTED_MODULE_3__["default"], null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_col__WEBPACK_IMPORTED_MODULE_5__[/* default */ "a"], {
+    span: 12
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_form__WEBPACK_IMPORTED_MODULE_11__[/* default */ "a"].Item, {
+    label: "col1"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_input__WEBPACK_IMPORTED_MODULE_7__[/* default */ "a"], null))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_col__WEBPACK_IMPORTED_MODULE_5__[/* default */ "a"], {
+    span: 12
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_form__WEBPACK_IMPORTED_MODULE_11__[/* default */ "a"].Item, {
+    label: "col2"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement(antd_es_input__WEBPACK_IMPORTED_MODULE_7__[/* default */ "a"], null)))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_14___default.a.createElement("h1", null, "4. \\u6307\\u5B9A\\u5177\\u4F53\\u7684\\u5BBD\\u5EA6"), \`
+          labelCol={{ flex: '0 0 100px' }} 
+          wrapperCol={{ flex: '0 0 100px' }} 
+          style={{ display: 'inline-flex', width: 'calc(45% - 4px)' }} 
+        \`));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (FormLayout);
+
+//# sourceURL=webpack:///./src/Components/Antd/FormLayout/index.tsx?`)}}]);
