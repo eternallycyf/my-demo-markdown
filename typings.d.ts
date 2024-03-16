19296279233153ecab01e46b68e9318d5b8915d0
eternallycyf <eternallycyf@eternallycyfMacBook-Pro.local>
2024-03-16 10:07:07 +0800

    🔖 chore: remove docx-preview

--- typings.d.ts
+++ typings.d.ts
@@ -4,7 +4,6 @@ declare module 'react-excel-renderer';
 declare module 'pizzip';
 declare module 'jszip-utils';
 declare module 'file-saver';
-declare module 'docxtemplater';
 declare module 'react-syntax-highlighter';
 declare module 'react-syntax-highlighter/dist/esm/styles/prism';
 declare module 'three';
